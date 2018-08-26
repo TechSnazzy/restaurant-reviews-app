ef6bdcb173039727d3ee488ee08db8a0939f0e3a
Sean Morrison <sean1972@gmail.com>
2018-08-26 10:09:48 -0700

    Updated dbhelper.js so that I can view page content when hosted on GitHub

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,12 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    // uncomment these 2 lines if hosting on local server
+    //const port = 8000 // Change this to your server port
+    //return `http://localhost:${port}/data/restaurants.json`;
+
+    // uncomment this line if hosting on GitHub Pages site.
+    return `http://techsnazzy.com/restaurant-reviews-app/data/restaurants.json`;
   }
 
   /**
@@ -157,7 +161,7 @@ class DBHelper {
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
+    // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
       alt: restaurant.name,
@@ -165,7 +169,7 @@ class DBHelper {
       })
       marker.addTo(newMap);
     return marker;
-  } 
+  }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
@@ -178,4 +182,3 @@ class DBHelper {
   } */
 
 }
-
